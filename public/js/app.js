b807b0b86b5a3971893b14a64515408d011fe7c4
Menor <deborah.menor@accenture.com>
2022-09-21 14:37:20 +0800

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log("Client side javaScript file is loaded!");
-
 // fetch("https://puzzle.mead.io/puzzle").then((response) => {
 //   response.json().then((data) => {
 //     console.log(data);
